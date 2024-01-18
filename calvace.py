31adc655f78596a98373a7063e3e44140a8c3e07
Abder <148877950+Abderreze@users.noreply.github.com>
2024-01-18 16:36:33 +0100

    Add files via upload

--- calvace.py
+++ calvace.py
@@ -5,14 +5,13 @@ with open("Characters.csv", mode='r', encoding='utf-8') as f:
     reader = csv.DictReader(f, delimiter=';')
     characters_tab = [{key : value.replace('\xa0', ' ') for key, value in element.items()} for element in reader]
 
-print(characters_tab)
 
 
 with open("Caracteristiques_des_persos.csv", mode='r', encoding='utf-8') as f:
     reader = csv.DictReader(f, delimiter=';')
     characteristics_tab = [{key : value for key, value in element.items()} for element in reader]
 
-print(characteristics_tab)
+
 
 poudlard_characters = []
 
@@ -22,9 +21,15 @@ for poudlard_character in characteristics_tab:
             poudlard_character.update(kaggle_character)
             poudlard_characters.append(poudlard_character)
 
-print(poudlard_characters)
+
 # rajouter un "k" = 5 pour connaitre les 5 plus proche voinsins
 charactere_test = {"Courage": 9, "Ambition": 2, "Intelligence": 8, "Good": 9}
 for perso in poudlard_characters:
-    chemin_cas_plus_proche = sqrt(((int(perso["Courage"]) - charactere_test["Courage"])**2) + (((int(perso["Ambition"]) - charactere_test["Ambition"])**2)) + (((int(perso["Intelligence"]) - charactere_test["Intelligence"])**2)) + (((int(perso["Good"]) - charactere_test["Good"])**2)))
-print(chemin_cas_plus_proche)
+    distance = sqrt(((int(perso["Courage"]) - charactere_test["Courage"])**2) + (((int(perso["Ambition"]) - charactere_test["Ambition"])**2)) + (((int(perso["Intelligence"]) - charactere_test["Intelligence"])**2)) + (((int(perso["Good"]) - charactere_test["Good"])**2)))
+    perso["Distance"] = distance
+poudlard_characters.sort(key=lambda x : x["Distance"])
+k_plus_proche_voisins = []
+for persos in poudlard_characters:
+    while len(k_plus_proche_voisins) < 5:
+        k_plus_proche_voisins.append(persos)
+print(k_plus_proche_voisins)
