d5f32ff7af532eda7d66befe27532730858b6af6
Abder <148877950+Abderreze@users.noreply.github.com>
2024-02-22 15:57:02 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,8 @@
+
+from kPPV_HENNI_ROUX_MALBERT import *
+from page import *
+
+
+
+table, perso_avec_maison  = teste_perso_voisins(poudlard_characters,profile_web)
+affichage_des_voisins(perso_avec_maison, table)
